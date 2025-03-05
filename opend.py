8090a1328f849341d8dc47415d0e1dab04523a38
aquiles021110 <aquiles@bousigues.com>
2025-03-05 15:19:34 +0100

    Update opend.py

--- opend.py
+++ opend.py
@@ -5,8 +5,10 @@ w=Tk()
 def opnfl():
     file=askopenfile(mode='r',filetypes=[('Text Doc','*.txt'),('Python','*.py')])
     if file is not None:
-        content=file.read()
-        print(content)
+        l.delete(0,END)
+        content=file.readlines()
+        for i in content:
+            l.insert(END,i)
     else:
         messagebox.showerror('Error','Pick a file')
 def svefl():
@@ -14,10 +16,23 @@ def svefl():
     file=asksaveasfile(filetypes=files,defaultextension=files)
     if file:
         print(e.get(),file=file)
+def delfl():
+    sopt=l.curselection()
+    if sopt:
+        l.delete(sopt)
+    else:
+        messagebox.showwarning('Oops','There is no current selection')
 opn=Button(w,text='Open',command=opnfl)
 opn.pack()
 sve=Button(w,text='Save',command=svefl)
 sve.pack()
 e=Entry(w)
 e.pack()
+dele=Button(w,text='Delete',command=delfl)
+dele.pack()
+l=Listbox(w,width=60,background='Grey')
+l.insert(END,'NO FILE')
+l.pack()
 mainloop()
+#$listbox.insert
+#add
